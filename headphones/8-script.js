28896745cb5e47f5224f5a035ab3fec1410491ab
MarcoIbrahim25 <marcoserry88@gmail.com>
2025-08-09 10:33:42 +0300

    New

--- headphones/8-script.js
+++ headphones/8-script.js
@@ -1,17 +1,3 @@
-const openMenu = document.querySelector(".holberton_school-icon-menu");
-const closeMenu = document.querySelector(".x");
-const mobileNav = document.querySelector(".mobile_nav");
-
-let isOpen = false;
-
-openMenu.addEventListener("click", function (e) {
-  mobileNav.classList.toggle("hidden");
-  openMenu.classList.add("hidden");
-  closeMenu.classList.remove("hidden");
-});
-
-closeMenu.addEventListener("click", function (e) {
-  mobileNav.classList.toggle("hidden");
-  openMenu.classList.remove("hidden");
-  closeMenu.classList.add("hidden");
+document.getElementById("hamburger").addEventListener("click", function () {
+ document.getElementById("overlay").classList.toggle("active");
 });
